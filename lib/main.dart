51f0a7cf22f40d20aadde6a00a5a621f292e6b57
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-01-30 23:17:33 +0530

    Created the Authenticate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:authentication_mobile_application/screens/wrapper.dart';
+import 'package:authentication_mobile_application/screens/authentication/authenticate.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Wrapper();
+    return const Authenticate();
   }
 }
