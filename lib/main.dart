5e4464191e7868f788d7ba5fe023a5e877d6e0e5
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-01-30 23:25:41 +0530

    Created the Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:authentication_mobile_application/screens/authentication/authenticate.dart';
+import 'package:authentication_mobile_application/screens/authentication/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Authenticate();
+    return const Login();
   }
 }
