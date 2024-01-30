c8e60e6ffbeb4996337f87b63848d06c81f83a22
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-01-30 23:34:05 +0530

    Created the Register

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:authentication_mobile_application/screens/authentication/login.dart';
+import 'package:authentication_mobile_application/screens/authentication/register.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Login();
+    return const Register();
   }
 }
---
+++ lib/screens/authentication/register.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Register"),
+        ),
+      ),
+    );
+  }
+}
