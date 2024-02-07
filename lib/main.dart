4b71fc229565f36eeb3a6ec1078a4ab9d6e0675b
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-02-07 09:25:22 +0530

    Changed some file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:authentication_mobile_application/screens/authentication/register.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
