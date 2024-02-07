dc9a7f22159a076f1a51057dee2229c75960812a
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-02-07 09:12:03 +0530

    Revert changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:authentication_mobile_application/screens/wrapper.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:authentication_mobile_application/screens/authentication/register.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
@@ -13,9 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Wrapper(),
-    );
+    return const Register();
   }
 }
