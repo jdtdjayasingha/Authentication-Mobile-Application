876adc43bc2952be292631f6544f2f6fd7aaa451
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-07-21 23:21:55 +0530

    Upgrade Flutter project to latest version

--- android/app/src/main/kotlin/com/example/authentication_mobile_application/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/authentication_mobile_application/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.authentication_mobile_application
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:authentication_mobile_application/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Wrapper(),
+      home: Scaffold(),
     );
   }
 }
